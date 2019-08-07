3deb3abcaeb53be091507eb074f604208f022d2f
Matevz Vucnik <vucnikm@gmail.com>
2019-08-07 14:14:55 +0200

    fix

--- pmc-parser.py
+++ pmc-parser.py
@@ -1,6 +1,14 @@
 
 start_time = 1559001600
 
+mqtt_topic = "saam/data/mihas/sens_power_"
+
+energy_json = {
+            "timestamp": 12345566,
+            "timestep": 3600,
+            "measurements": [ 234.5 ]
+            }
+
 for x in range(24):
     file_name = "out_by_app_f1_" + str(x) + ".csv"
     print(file_name)
@@ -11,20 +19,40 @@ for x in range(24):
     with open(file_name, "r") as ins:
         for line in ins:
             if "dump" not in line:
-                print(line.strip())
-                # TODO if energy send energy json
-                # else send event json
+                if "Energy" in line:
+                    energy_json["timestamp"] = start_time
+                    energy_json["measurements"] = [float(line.strip().split(':')[1])]
+                    print(mqtt_topic + "f1_" + "energy")
+                    print(energy_json)
+                    print(line.strip())
+                else:
+                    # TODO event json
+                    print(line.strip())
 
     file_name = "out_by_app_f2_" + str(x) + ".csv"
 
     with open(file_name, "r") as ins:
         for line in ins:
             if "dump" not in line:
-                print(line.strip())
+                if "Energy" in line:
+                    energy_json["timestamp"] = start_time
+                    energy_json["measurements"] = [float(line.strip().split(':')[1])]
+                    print(mqtt_topic + "f2_" + "energy")
+                    print(energy_json)
+                    print(line.strip())
+                else:
+                    print(line.strip())
 
     file_name = "out_by_app_f3_" + str(x) + ".csv"
 
     with open(file_name, "r") as ins:
         for line in ins:
             if "dump" not in line:
-                print(line.strip())
+                if "Energy" in line:
+                    energy_json["timestamp"] = start_time
+                    energy_json["measurements"] = [float(line.strip().split(':')[1])]
+                    print(mqtt_topic + "f3_" + "energy")
+                    print(energy_json)
+                    print(line.strip())
+                else:
+                    print(line.strip())
