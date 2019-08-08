a2c16fd83e0e74b8b1cfc19c607eda477c2a6d89
Matevz Vucnik <vucnikm@gmail.com>
2019-08-08 11:10:09 +0200

    fix

--- pmc-parser.py
+++ pmc-parser.py
@@ -1,3 +1,8 @@
+import json
+import paho.mqtt.client as mqtt
+
+client = mqtt.Client()
+client.connect("localhost")
 
 start_time = 1559001600
 
@@ -13,13 +18,11 @@ energy_json = {
 event_json = {
             "timestamp": 12345566,
             "timestep": -1,
-            "measurements": [ {"deltaP": 23.5, "deltaQ":-12.6} ]
+            "measurements": [ {"dP": 23.5, "dQ":-12.6} ]
 }
 
 for x in range(24):
     file_name = "out_by_app_f1_" + str(x) + ".csv"
-    print(file_name)
-    print(start_time)
 
     with open(file_name, "r") as ins:
         for line in ins:
@@ -27,20 +30,24 @@ for x in range(24):
                 if "Energy" in line:
                     energy_json["timestamp"] = start_time
                     energy_json["measurements"] = [float(line.strip().split(':')[1])]
-                    print(mqtt_topic + "f1_" + "energy")
-                    print(energy_json)
-                    #print(line.strip())
+
+                    t = mqtt_topic + "f1_" + "energy"
+                    print(t)
+                    print(json.dumps(energy_json))
+                    client.publish(topic=t, payload=json.dumps(energy_json))
+
                 else:
                     l = line.strip().split(':')
                     event = l[0]
                     d = l[1].split(',')
 
                     event_json["timestamp"] = int(int(d[0])/1000)
-                    event_json["measurements"] = [ {"deltaP": float(d[1]), "deltaQ": float(d[2])} ] 
+                    event_json["measurements"] = [ {"dP": float(d[1]), "dQ": float(d[2])} ] 
 
-                    print(mqtt_topic_event + "f1_" + line.strip().split(':')[0])
-                    print(event_json)
-                    #print(line.strip())
+                    t = mqtt_topic_event + "f1_" + line.strip().split(':')[0]
+                    print(t)
+                    print(json.dumps(event_json))
+                    client.publish(topic=t, payload=json.dumps(event_json))
 
     file_name = "out_by_app_f2_" + str(x) + ".csv"
 
@@ -50,20 +57,24 @@ for x in range(24):
                 if "Energy" in line:
                     energy_json["timestamp"] = start_time
                     energy_json["measurements"] = [float(line.strip().split(':')[1])]
-                    print(mqtt_topic + "f2_" + "energy")
-                    print(energy_json)
-                    #print(line.strip())
+
+                    t = mqtt_topic + "f2_" + "energy"
+                    print(t)
+                    print(json.dumps(energy_json))
+                    client.publish(topic=t, payload=json.dumps(energy_json))
+
                 else:
                     l = line.strip().split(':')
                     event = l[0]
                     d = l[1].split(',')
 
                     event_json["timestamp"] = int(int(d[0])/1000)
-                    event_json["measurements"] = [ {"deltaP": float(d[1]), "deltaQ": float(d[2])} ] 
+                    event_json["measurements"] = [ {"dP": float(d[1]), "dQ": float(d[2])} ] 
 
-                    print(mqtt_topic_event + "f2_" + line.strip().split(':')[0])
-                    print(event_json)
-                    #print(line.strip())
+                    t = mqtt_topic_event + "f2_" + line.strip().split(':')[0]
+                    print(t)
+                    print(json.dumps(event_json))
+                    client.publish(topic=t, payload=json.dumps(event_json))
 
     file_name = "out_by_app_f3_" + str(x) + ".csv"
 
@@ -73,19 +84,23 @@ for x in range(24):
                 if "Energy" in line:
                     energy_json["timestamp"] = start_time
                     energy_json["measurements"] = [float(line.strip().split(':')[1])]
-                    print(mqtt_topic + "f3_" + "energy")
-                    print(energy_json)
-                    #print(line.strip())
+
+                    t = mqtt_topic + "f3_" + "energy"
+                    print(t)
+                    print(json.dumps(energy_json))
+                    client.publish(topic=t, payload=json.dumps(energy_json))
+
                 else:
                     l = line.strip().split(':')
                     event = l[0]
                     d = l[1].split(',')
 
                     event_json["timestamp"] = int(int(d[0])/1000)
-                    event_json["measurements"] = [ {"deltaP": float(d[1]), "deltaQ": float(d[2])} ] 
+                    event_json["measurements"] = [ {"dP": float(d[1]), "dQ": float(d[2])} ] 
 
-                    print(mqtt_topic_event + "f3_" + line.strip().split(':')[0])
-                    print(event_json)
-                    #print(line.strip())
+                    t = mqtt_topic_event + "f3_" + line.strip().split(':')[0]
+                    print(t)
+                    print(json.dumps(event_json))
+                    client.publish(topic=t, payload=json.dumps(event_json))
 
     start_time = start_time + 3600
